a1b39f3c512e43b58e0c3dc22f9243ccfba504b5
b-hyoung <youqlrqod@naver.com>
2022-08-03 21:21:01 +0900

    add Page IsLogin & Clover & SearchClub & Login

---
+++ frontend/src/Pages/SearchClubPage/SearchClubPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './SearchClubPage.css'
+
+function SearchClubPage() {
+  return (
+    <div className='Wrapper_Search'>
+        <header className='Search_header'>Find An Club</header>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default SearchClubPage
